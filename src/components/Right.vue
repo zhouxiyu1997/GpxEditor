b931c597b835944bbdbf1d6a5c01526e42735571
Moonlit1997 <zhouxiyu1997@gmail.com>
2024-03-07 16:42:25 +0800

    tailwind

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="right">
+  <div class="right  flex-1 flex flex-col">
     right <el-button type="primary">切换底图</el-button>
     <el-button @click="ZoomIn"> <Plus style="width: 1em; height: 1em; margin-right: 8px" /></el-button>
     <el-button @click="ZoomOut"> <Minus style="width: 1em; height: 1em; margin-right: 8px" /></el-button>
