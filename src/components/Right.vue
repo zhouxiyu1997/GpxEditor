5e2670cb74dc900433a866bca09c265a4faceb9c
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-03-09 18:35:08 +0800

    Revert "update: readme and license"
    This reverts commit b36913c090e301faff2e86120dda8f3f99f2c5db.

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -21,6 +21,8 @@ const intervalId = setInterval(() => {
 }, 1000); // 每100毫秒检查一次
 const ZoomIn = () => {
   window.earthViewer.scene.camera.zoomIn();
+  console.log(earthViewer);
+  console.log(window.earthViewer);
 };
 const ZoomOut = () => {
   earthViewer.scene.camera.zoomOut();
