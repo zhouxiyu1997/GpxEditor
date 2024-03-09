ddc833890273fb887423118062bbfc3492816b89
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-03-09 18:34:54 +0800

    Revert "Merge pull request #3 from Woodii1998/pref/readme"
    This reverts commit 8ccc50d9c606f25d8dc90704db58892b816c4d91, reversing
    changes made to 04b21c0e80d36a60af7050ddd417edeac82fdfcd.

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,8 +33,8 @@ module.exports = {
   },
 
   rules: {
-    'no-console': 'error',
-    'no-debugger': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
 
     'vue/multi-word-component-names': 'off',
   },
