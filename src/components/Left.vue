f120e8b93c8754dfe34822498a5cc6563bf980f6
Moonlit1997 <zhouxiyu1997@gmail.com>
2024-03-14 15:10:01 +0800

    feat:id byscope

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="left flex flex-col">
     <el-button @click="startEdit">点击编辑(TODO)</el-button>
-    <el-button @click="startDelectById(100, 300)">点击删除</el-button>
-    <el-button @click="startDelectById(0, 100)">根据id删除</el-button>
+    <el-button @click="startDelectById(0)">点击删除(TODO)</el-button>
+    <el-input v-model="inputID" style="width: 100px" placeholder="0-100" />
+    <el-button @click="startDelectById()">根据id删除</el-button>
     <el-button @click="until">框选删除(TODO)</el-button>
   </div>
 </template>
 <script setup>
 // 引用cesium
 import * as Cesium from 'cesium/Build/Cesium';
+import { ref } from 'vue';
 import { until, delectGpx, drawGpx, countGpx } from '../api/until';
 let earthViewer;
+let inputID = ref('');
 const intervalId = setInterval(() => {
   if (window.earthViewer) {
     earthViewer = window.earthViewer;
@@ -38,7 +41,10 @@ const startEdit = () => {
   };
   earthViewer.scene.camera.setView(homeCameraView);
 };
-const startDelectById = (index, index2) => {
+const startDelectById = () => {
+  //读取inputID.value，设为index,index2
+  let index = parseInt(inputID.value.split('-')[0]);
+  let index2 = parseInt(inputID.value.split('-')[1]);
   let positions = delectGpx(index, index2);
   //todo更新到兄弟组件
   console.log(countGpx(positions));
